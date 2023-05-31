eef01ab76d01910a3e59c5d437272fb81b81b722
CrizpyC <dchang0413@gmail.com>
2023-05-30 20:12:29 -0500

    fafnir helm, striking miko, striking mail

--- community_iteminfo.lua
+++ community_iteminfo.lua
@@ -1,9 +1,83 @@
 community_iteminfo = {
 
     -- Red Potion
-    [501]    = { 
+    [501]   = { 
         identifiedDescriptionName = {
             "Yep, that's a Red Potion."
         } 
     },
+    [400177]    = {
+        identifiedDescriptionName = {
+            "Reduce After Cast Delay by 10%",
+            "Increase damage of <sk>Dragon's Breath</sk> and <sk>Dragon's Water Breath</sk> by 20%",
+            "Increase damage of <sk>Storm Blast</sk> by 30%",
+            "Reduce Fixed Cast Time of Nosiege Rune by 100%<lb>",
+            "<g>For every refine<bla>",
+            "Increase resistance to Fire and Water by 3%<lb>",
+            "If <sk>Lv. 5 Dragon Training</sk> is learned",
+            "MaxHP+ 25%, MaxSP+ 25%, Reduce after cast delay by 20%<lb>",
+            "If <sk>Lv. 5 Dragon Howling</sk> is learned",
+            "Reduce damage taken from normal and boss class by 20%<lb>",
+            "If <sk>Lv. 10 Dragon's Breath</sk> and <sk>Lv. 10 Dragon's Water Breath</sk> are learned",
+            "Increase ranged damage by 50%",
+            "Reduce Fixed Cast Time of <sk>Dragon's Breath</sk> and <sk>Dragon's Water Breath</sk> by 0.2 sec<lb>",
+            "<g>When refined to +6 or higher<bla>",
+            "Increase damage of <sk>Storm Blast</sk> by 20%<lb>",
+            "<g>When refined to +7 or higher<bla>",
+            "Increase damage of <sk>Dragon's Breath</sk> and <sk>Dragon's Water Breath</sk> by 30%<lb>",
+            "<g>When refined to +8 or higher<bla>",
+            "Increase damage of <sk>Storm Blast</sk> by 20%<lb>",
+            "<g>When refined to +9 or higher<bla>",
+            "Increase damage of <sk>Dragon's Breath</sk> and <sk>Dragon's Water Breath</sk> by 50%<lb>",
+            "<g>When using Turisus Rune<bla>",
+            "For 4 minutes, STR+ 30, INT- 30<lb>",
+            "<g>When using Isia Rune<bla>",
+            "For 5 minutes, Recover 500 HP every 10 seconds<lb>",
+            "<g>When using Hagalas Rune<bla>",
+            "For 5 minutes, Reflects 10% of incoming melee damage<lb>",
+            "<g>When using Asir Rune<bla>",
+            "For 5 minutes, ATK+ 10, ASPD+ 10%<lb>",
+            "<g>When using Urj Rune<bla>",
+            "For 5 minutes, Recover 40 SP every 10 seconds<lb>",
+            "<g>When using Rhydo Rune<bla>",
+            "For 30 seconds, Resist Knockback<lb>",
+            "<g>When using Nosiege Rune<bla>",
+            "Recover 100% HP and SP, Cleanses all Masquerade status<lb>",
+            "<g>When using Verkana Rune<bla>",
+            "Reduce physical damage to all class by 50%, Reduce ranged damage by 50%, MATK- 50%",
+            "Adds a chance to autocast <sk>Lv1. Millenium Shield</sk>",
+            "Chance to autocast is increased when helmet refinement is above +6 and +8",
+            "<r>This effect does not work in WoE maps<bla><lb>",
+
+        } 
+    },
+    [450165]    = {
+        identifiedDescriptionName = {
+            "All Stats +10<lb>",
+            "<g>When equipped with Striking Mikoshi<bla>",
+            "For every 3 refine of Striking Mail",
+            "Max HP+ 20% and reduce ranged damage taken by 5%<lb>",
+        } 
+    },
+    [480114]    = {
+        identifiedDescriptionName = {
+            "Reduce physical damage from normal, boss and player class by 5%<lb>",
+            "<g>For every refine<bla>",
+            "Reduce after cast delay by 1%",
+            "Increase physical damage to all class by 2%<lb>",
+            "<g>When refined to +6 or higher<bla>",
+            "Increase ASPD by 10%, Perfect Hit by 25<lb>",
+            "<g>When refined to +8 or higher<bla>",
+            "Enables <sk>Lv.10 Improve Concentration</sk>",
+            "If <sk>Lv.10 Improve Concentration</sk> is learned, ATK+ 50, Flee+ 50<lb>",
+            "<g>When equipped with Striking Hat<bla>",
+            "Increase ranged damage by 25%, recover 5 SP per attack<lb>",
+            "<g>When equipped with Striking Shoes<bla>",
+            "For every 3 refine of Striking Shoes",
+            "All Stats +10<lb>",
+            "<g>When equipped with Striking Mail<bla>",
+            "For every 3 refine of Striking Mail",
+            "Max HP+ 20% and reduce ranged damage taken by 5%<lb>",
+        } 
+    }
 }
